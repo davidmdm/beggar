d901c42b2b1ac1bb36b701336a344eea5621f4b2
davidmdm <david.desmarais-michaud@pj.ca>
2019-10-26 21:50:48 -0400

    setup testing server

---
+++ test/server.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const http = require('http');
+const { Readable } = require('stream');
+
+const getAllDataFromReadable = readable => {
+  if (!(readable instanceof Readable)) {
+    throw new TypeError('Value must be an instance of stream.Readable');
+  }
+  return new Promise((resolve, reject) => {
+    let buffer = Buffer.from([]);
+    readable
+      .on('data', data => (buffer = Buffer.concat([buffer, data])))
+      .on('end', () => resolve(buffer))
+      .on('error', reject);
+  });
+};
+
+http
+  .createServer((req, res) => {
+    if (req.url === '/home') {
+      return res.writeHead(200, { 'Content-Type': 'text/plain' }).end('Welcome to the homepage');
+    }
+
+    if (req.url === '/echo') {
+      return req.pipe(res);
+    }
+
+    if (req.url === '/redirect') {
+      res.setHeader('Location', '/home');
+      res.statusCode = 302;
+      return res.end();
+    }
+
+    if (req.url.startsWith('/details')) {
+      return getAllDataFromReadable(req).then(buffer => {
+        res.writeHead(200, { 'Content-Type': 'application/json' }).end(
+          JSON.stringify({
+            request: {
+              method: req.method,
+              path: req.url,
+              headers: req.headers,
+              body: buffer && buffer.toString(),
+            },
+          })
+        );
+      });
+    }
+
+    return res.writeHead(404).end();
+  })
+  .listen(3000, () => console.log('listening on port 3000'));
