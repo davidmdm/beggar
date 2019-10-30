938cbd0c53028e098d556e3adb52f89c964b8e99
davidmdm <david.desmarais-michaud@pj.ca>
2019-10-29 22:25:33 -0400

    updated ResolvedResponse type

--- index.d.ts
+++ index.d.ts
@@ -4,42 +4,42 @@ import { IncomingMessage } from 'http';
 import { Readable, Duplex } from 'stream';
 declare type Dictionary<T> = Partial<Record<string, T>>;
 export declare type RequestOptions = {
-    method?: string;
-    uri: string | URL;
-    json?: boolean;
-    followRedirects?: boolean;
-    headers?: Dictionary<string | string[]>;
-    auth?: {
-        user: string;
-        pass: string;
-    };
-    body?: Buffer | string | Object;
-    form?: Dictionary<any>;
-    formData?: Dictionary<string | Buffer | Readable>;
+  method?: string;
+  uri: string | URL;
+  json?: boolean;
+  followRedirects?: boolean;
+  headers?: Dictionary<string | string[]>;
+  auth?: {
+    user: string;
+    pass: string;
+  };
+  body?: Buffer | string | Object;
+  form?: Dictionary<any>;
+  formData?: Dictionary<string | Buffer | Readable>;
 };
 export declare type ResolvedResponse = IncomingMessage & {
-    body: string;
-    redirects?: string[];
+  body: any;
+  redirects?: string[];
 };
 export declare type Connection = Duplex & {
-    then: <T>(fn: (response: ResolvedResponse) => T) => Promise<T>;
-    catch: <T>(handle: (err: Error) => T) => Promise<ResolvedResponse | T>;
+  then: <T>(fn: (response: ResolvedResponse) => T) => Promise<T>;
+  catch: <T>(handle: (err: Error) => T) => Promise<ResolvedResponse | T>;
 };
 declare type RequestOptionsWithoutMethod = Omit<RequestOptions, 'method'>;
 declare type RequestFn = {
-    (options: RequestOptions): Connection;
-    (uri: string | URL | RequestOptions, options?: Omit<RequestOptions, 'uri'>): Connection;
+  (options: RequestOptions): Connection;
+  (uri: string | URL | RequestOptions, options?: Omit<RequestOptions, 'uri'>): Connection;
 };
 declare type MethodlessRequestFn = {
-    (options: RequestOptionsWithoutMethod): Connection;
-    (uri: string | URL | RequestOptionsWithoutMethod, options?: Omit<RequestOptionsWithoutMethod, 'uri'>): Connection;
+  (options: RequestOptionsWithoutMethod): Connection;
+  (uri: string | URL | RequestOptionsWithoutMethod, options?: Omit<RequestOptionsWithoutMethod, 'uri'>): Connection;
 };
 export declare const request: RequestFn & {
-    get: MethodlessRequestFn;
-    post: MethodlessRequestFn;
-    put: MethodlessRequestFn;
-    patch: MethodlessRequestFn;
-    head: MethodlessRequestFn;
-    delete: MethodlessRequestFn;
+  get: MethodlessRequestFn;
+  post: MethodlessRequestFn;
+  put: MethodlessRequestFn;
+  patch: MethodlessRequestFn;
+  head: MethodlessRequestFn;
+  delete: MethodlessRequestFn;
 };
 export {};
