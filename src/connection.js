e4af1070dc5dba5fb23720575a899d18518c078f
davidmdm <david.desmarais-michaud@pj.ca>
2020-02-20 00:30:46 -0500

    use statusOK function

--- src/connection.js
+++ src/connection.js
@@ -150,7 +150,7 @@ class Connection extends Duplex {
         }
         const response = this.incomingMessage || (await new Promise(resolve => this.once('response', resolve)));
         const buffer = await readableToBuffer(this);
-        if (this.opts.rejectError === true && response.statusCode >= 400) {
+        if (this.opts.rejectError === true && !statusOk(response.statusCode)) {
           this.responseError = getResponseError(response, buffer);
           throw this.responseError;
         }
